31f6719d8c00f34c40dd397696f0b6b6cb060893
KavinSharon <69906375+KavinSharon@users.noreply.github.com>
2020-10-24 19:18:03 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -21,10 +21,26 @@ function setup() {
 
 function draw() {  
   background(46,139,87);
+  if(keyWentDown(UP_ARROW)){
+    writeStock(foodS)
+    dog.addImage(happyDogImage);
+  }
   drawSprites();
+  text("Note:Press Up Arrow Key To Feed Your Dog Drago Milk",100,30);
   //add styles here
-
+  
+}
+function readStock(data){
+  foodS = data.val();
 }
 
-
-
+function writeStock(x){
+  if(x<=0){
+    x=0;
+  }else{
+    x=x-1;
+  }
+  database.ref('/').update({
+    Food:x
+  })
+}
